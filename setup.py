b7652cc29c9f7da3b9df3565ddf71f47f93978ad
Paul Water <p.r.water@setbased.nl>
2020-10-25 00:45:01 +0200

    Release: 1.0.2

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
 setup(
     name='PyStratum-Common',
 
-    version='1.0.1',
+    version='1.0.2',
 
     description='PyStratum Common: Common parts shared between backends for database systems',
     long_description=long_description,
